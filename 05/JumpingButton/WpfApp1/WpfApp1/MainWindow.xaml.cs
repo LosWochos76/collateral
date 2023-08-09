57081f0e539ad4ed04e2c578f0e0503822d643ac
unknown <alexander.stuckenholz@gmail.com>
2023-08-09 15:56:03 +0200

    Working

---
+++ 05/JumpingButton/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1;
+
+public partial class MainWindow : Window
+{
+    private Random rnd = new Random();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_MouseEnter(object sender, MouseEventArgs e)
+    {
+        var next_x = rnd.NextDouble() * (canvas.ActualWidth - button.ActualWidth);
+        var next_y = rnd.NextDouble() * (canvas.ActualHeight - button.ActualHeight);
+        Canvas.SetTop(button, next_y);
+        Canvas.SetLeft(button, next_x);
+    }
+
+    private void button_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
