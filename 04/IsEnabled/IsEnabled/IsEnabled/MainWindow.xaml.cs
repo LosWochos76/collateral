7407928492abc562cee04bd519d16b2c4973d947
unknown <alexander.stuckenholz@gmail.com>
2023-08-10 09:45:41 +0200

    Working

---
+++ 04/IsEnabled/IsEnabled/IsEnabled/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IsEnabled;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void text_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        MailAddress addr;
+        if (MailAddress.TryCreate(text.Text, out addr))
+            button.IsEnabled = true;
+        else
+            button.IsEnabled = false;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
--- 04/Pizza/Pizza/WpfApp1/MainWindow.xaml.cs
+++ 04/Pizza/Pizza/WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var belaege = new List<string>();
+        foreach (var control in panel.Children)
         {
-            InitializeComponent();
+            var obj = control as CheckBox;
+            if (obj != null && obj.IsChecked.HasValue && obj.IsChecked.Value)
+                belaege.Add(obj.Content.ToString());
         }
+
+        var belaege_als_string = string.Join(", ", belaege.ToArray());
+        MessageBox.Show("Beläge: " + belaege_als_string);
     }
 }
