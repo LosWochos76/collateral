28708e44f5be4aeae5f319c28aeb568dda8c80b7
unknown <alexander.stuckenholz@gmail.com>
2023-08-14 14:48:26 +0200

    Working

---
+++ 06/Pong/Pong/Pong/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pong;
+
+public partial class MainWindow : Window
+{
+    private double delta_x = 5;
+    private double delta_y = 5;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var dp = new DispatcherTimer();
+        dp.Interval = new TimeSpan(0, 0, 0, 0, 5);
+        dp.Tick += Dp_Tick;
+        dp.Start();
+    }
+
+    private void Dp_Tick(object? sender, EventArgs e)
+    {
+        var x = Canvas.GetLeft(ball);
+        var y = Canvas.GetTop(ball);
+
+        if (x >= canvas.ActualWidth - ball.ActualWidth || x <= 0)
+            delta_x *= -1;
+
+        if (y >= canvas.ActualHeight - ball.ActualHeight || y <= 0)
+            delta_y *= -1;
+
+        x += delta_x;
+        y += delta_y;
+
+        Canvas.SetLeft(ball, x);
+        Canvas.SetTop(ball, y);
+    }
+}
---
+++ 06/Watch/Watch/Watch/WatchControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Watch;
+
+public partial class WatchControl : UserControl
+{
+    private const double DegToRad = Math.PI / 180;
+
+    public WatchControl()
+    {
+        InitializeComponent();
+
+        var dp = new DispatcherTimer();
+        dp.Interval = new System.TimeSpan(0, 0, 0, 0, 100);
+        dp.Tick += Dp_Tick;
+        dp.Start();
+
+        UpdateClock();
+    }
+
+    private void Dp_Tick(object? sender, System.EventArgs e)
+    {
+        UpdateClock();
+    }
+
+    private void UpdateClock()
+    {
+        var current = DateTime.Now;
+
+        var degree = (current.Hour % 12) * 30 + ((double)current.Minute / 60) * 30;
+        hour.X2 = 200 + 100 * Math.Sin(DegToRad * degree);
+        hour.Y2 = 200 - 100 * Math.Cos(DegToRad * degree);
+
+        degree = current.Minute * 6 + ((double)current.Second / 60) * 6;
+        minute.X2 = 200 + 150 * Math.Sin(DegToRad * degree);
+        minute.Y2 = 200 - 150 * Math.Cos(DegToRad * degree);
+
+        degree = current.Second * 6;
+        second.X2 = 200 + 200 * Math.Sin(DegToRad * degree);
+        second.Y2 = 200 - 200 * Math.Cos(DegToRad * degree);
+    }
+}
