cfc64e1d30b532c79f1e680de0a80b0e0595c682
unknown <alexander.stuckenholz@gmail.com>
2023-08-15 17:12:18 +0200

    Working

---
+++ 06/Skia/Skia/Skia/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using System;
+using System.Windows;
+
+namespace Skia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SKElement_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+    {
+        var canvas = e.Surface.Canvas;
+        var width = canvas.DeviceClipBounds.Width;
+        var height = canvas.DeviceClipBounds.Height;
+
+        var rnd = new Random();
+        for (int i=0; i<10000; i++)
+        {
+            var color = new SKColor(
+                (byte)rnd.Next(0, 255),
+                (byte)rnd.Next(0, 255),
+                (byte)rnd.Next(0, 255));
+
+            var paint = new SKPaint() { Color=color };
+            canvas.DrawLine(
+                rnd.Next(0, width), rnd.Next(0, height), 
+                rnd.Next(0, width), rnd.Next(0, height), paint);
+        }
+    }
+}
---
+++ 06/WatchTransform/Watch/Watch/WatchControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Watch;
+
+public partial class WatchControl : UserControl
+{
+    private const double DegToRad = Math.PI / 180;
+
+    public WatchControl()
+    {
+        InitializeComponent();
+
+        var dp = new DispatcherTimer();
+        dp.Interval = new System.TimeSpan(0, 0, 0, 0, 100);
+        dp.Tick += Dp_Tick;
+        dp.Start();
+
+        UpdateClock();
+    }
+
+    private void Dp_Tick(object? sender, System.EventArgs e)
+    {
+        UpdateClock();
+    }
+
+    private void UpdateClock()
+    {
+        var current = DateTime.Now;
+        hour.Angle = (current.Hour % 12) * 30 + ((double)current.Minute / 60) * 30;
+        minute.Angle = current.Minute * 6 + ((double)current.Second / 60) * 6;
+        second.Angle = current.Second * 6;
+    }
+}
