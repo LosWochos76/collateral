8956869a3efcaf542977c91fc596ec2ad6301d3b
unknown <alexander.stuckenholz@gmail.com>
2023-08-16 18:06:04 +0200

    Working

---
+++ 07/UserFormDemo/UserFormDemo/UserFormDemo/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserFormDemo.Model;
+
+public class User
+{
+    private string vorname;
+    private string nachname;
+
+    public string Vorname
+    {
+        get { return vorname;}
+        set { vorname = value; }
+    }
+
+    public string Nachname
+    {
+        get { return nachname; }
+        set { nachname = value; }
+    }
+
+    public User Clone()
+    {
+        return new User() { Vorname = Vorname, Nachname = Nachname };
+    }
+
+    public override string ToString()
+    {
+        return Vorname + " " + Nachname;
+    }
+}
---
+++ 07/UserFormDemo/UserFormDemo/UserFormDemo/UserDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using UserFormDemo.Model;
+
+namespace UserFormDemo;
+
+public partial class UserDialog : Window
+{
+    public UserDialog(User user)
+    {
+        InitializeComponent();
+        DataContext = user;
+    }
+
+    private void Button_Ok_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+
+    private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+}
