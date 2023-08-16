742b4062fc61d322461208a302d02c4424e61234
unknown <alexander.stuckenholz@gmail.com>
2023-08-16 15:16:35 +0200

    Working

---
+++ 07/IValueConverterDemo/IValueConverterDemo/IValueConverterDemo/Celsius2FahrenheitConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace IValueConverterDemo;
+
+class Celsius2FahrenheitConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return System.Convert.ToDouble(value) * 1.8 + 32;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (System.Convert.ToDouble(value) - 32) / 1.8;
+    }
+}
