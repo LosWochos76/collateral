26ed67376848cdfb3ed5f7d69720dc97059ba85c
unknown <alexander.stuckenholz@gmail.com>
2023-08-21 10:57:25 +0200

    Working

---
+++ 08/UserControlDemo/UserControlDemo/UserControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace UserControlDemo;
+
+public partial class MainWindow : Window
+{
+    public User CurrentUser { get; set; } = new User() 
+    { 
+        Vorname = "Peter", 
+        Nachname = "Müller", 
+        EMail = "peter.mueller@hshl.de" 
+    };
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = CurrentUser;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        CurrentUser.Vorname = "Michael";
+        CurrentUser.Nachname = "Meier";
+        CurrentUser.EMail = "michael.meier@hshl.de";
+    }
+}
---
+++ 08/UserControlDemo/UserControlDemo/UserControlDemo/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UserControlDemo;
+
+public class User : INotifyPropertyChanged
+{
+    private string vorname = string.Empty;
+    private string nachname = string.Empty;
+    private string email = string.Empty;
+
+    public string Vorname
+    {
+        get { return vorname; }
+        set 
+        { 
+            vorname = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public string Nachname
+    {
+        get { return nachname; }
+        set
+        {
+            nachname = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public string EMail
+    {
+        get { return email; }
+        set
+        {
+            email = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
