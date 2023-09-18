694fdb11f34035315d507b92d3e88083d5b37d94
unknown <alexander.stuckenholz@gmail.com>
2023-09-18 17:33:10 +0200

    Workgin

--- 14/WpfMailer/WpfMailer/SmtpMailer.cs
+++ 14/WpfMailer/WpfMailer/SmtpMailer.cs
@@ -26,65 +26,65 @@ namespace WpfMailer
             return Task.Factory.StartNew(() => SendMail(to, subject, message));
         }
 
-public bool SendMail(string to, string subject, string message)
-{
-    using (TcpClient mail = new TcpClient())
-    {
-        mail.Connect(host, port);
-
-        using (SslStream sslStream = new SslStream(mail.GetStream()))
+        public bool SendMail(string to, string subject, string message)
         {
-            sslStream.AuthenticateAsClient(host);
+            using (TcpClient mail = new TcpClient())
+            {
+                mail.Connect(host, port);
 
-            if (!ReadString(sslStream).Contains("ESMTP"))
-                return false;
+                using (SslStream sslStream = new SslStream(mail.GetStream()))
+                {
+                    sslStream.AuthenticateAsClient(host);
 
-            WriteString(sslStream, "EHLO example.net");
-            if (!ReadString(sslStream).Contains("AUTH LOGIN"))
-                return false;
+                    if (!ReadString(sslStream).Contains("ESMTP"))
+                        return false;
 
-            WriteString(sslStream, "AUTH LOGIN");
-            ReadString(sslStream);
-            WriteStringBase64(sslStream, username);
-            ReadString(sslStream);
-            WriteStringBase64(sslStream, password);
+                    WriteString(sslStream, "EHLO example.net");
+                    if (!ReadString(sslStream).Contains("AUTH LOGIN"))
+                        return false;
 
-            if (!ReadString(sslStream).ToUpper().Contains("ACCEPTED"))
-                return false;
+                    WriteString(sslStream, "AUTH LOGIN");
+                    ReadString(sslStream);
+                    WriteStringBase64(sslStream, username);
+                    ReadString(sslStream);
+                    WriteStringBase64(sslStream, password);
 
-WriteString(sslStream, "MAIL FROM:<" + username + ">");
-if (!ReadString(sslStream).Contains("OK"))
-    return false;
+                    if (!ReadString(sslStream).ToUpper().Contains("ACCEPTED"))
+                        return false;
 
-            WriteString(sslStream, "RCPT TO:<" + to + ">");
-            if (!ReadString(sslStream).Contains("OK"))
-                return false;
+                    WriteString(sslStream, "MAIL FROM:<" + username + ">");
+                    if (!ReadString(sslStream).Contains("OK"))
+                        return false;
 
-            WriteString(sslStream, "DATA");
-            if (!ReadString(sslStream).ToUpper().Contains("GO AHEAD"))
-                return false;
+                    WriteString(sslStream, "RCPT TO:<" + to + ">");
+                    if (!ReadString(sslStream).Contains("OK"))
+                        return false;
 
-            WriteString(sslStream, "From: <" + username + ">\r\n" +
-                "To: <" + to + ">\r\n" +
-                "Subject: " + subject + "\r\n" +
-                "\r\n" + message + "\r\n.\r\n");
+                    WriteString(sslStream, "DATA");
+                    if (!ReadString(sslStream).ToUpper().Contains("GO AHEAD"))
+                        return false;
 
-            if (!ReadString(sslStream).Contains("OK"))
-                return false;
+                    WriteString(sslStream, "From: <" + username + ">\r\n" +
+                        "To: <" + to + ">\r\n" +
+                        "Subject: " + subject + "\r\n" +
+                        "\r\n" + message + "\r\n.\r\n");
 
-            WriteString(sslStream, "QUIT");
+                    if (!ReadString(sslStream).Contains("OK"))
+                        return false;
+
+                    WriteString(sslStream, "QUIT");
+                }
+            }
+
+            return true;
         }
-    }
 
-    return true;
-}
-
-private string ReadString(SslStream stream)
-{
-    byte[] buffer = new byte[2048];
-    int bytes = stream.Read(buffer, 0, buffer.Length);
-    return Encoding.ASCII.GetString(buffer, 0, bytes);
-}
+        private string ReadString(SslStream stream)
+        {
+            byte[] buffer = new byte[2048];
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            return Encoding.ASCII.GetString(buffer, 0, bytes);
+        }
 
         private void WriteString(SslStream stream, string s)
         {
