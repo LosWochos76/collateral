0d06655535fda6eda925c6117c0226857103a56b
unknown <alexander.stuckenholz@gmail.com>
2023-09-19 14:27:39 +0200

    Working

--- 14/Bundesliga/Bundesliga/Match.cs
+++ 14/Bundesliga/Bundesliga/Match.cs
@@ -1,33 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Bundesliga
+﻿public class Match
 {
-    public class Match
-    {
-        public Match()
-        {
-            MatchResults = new List<MatchResult>();
-        }
+    public int matchID { get; set; }
+    public DateTime matchDateTime { get; set; }
+    public string timeZoneID { get; set; }
+    public int leagueId { get; set; }
+    public string leagueName { get; set; }
+    public int leagueSeason { get; set; }
+    public string leagueShortcut { get; set; }
+    public DateTime matchDateTimeUTC { get; set; }
+    public Group group { get; set; }
+    public Team Team1 { get; set; }
+    public Team Team2 { get; set; }
+    public DateTime lastUpdateDateTime { get; set; }
+    public bool matchIsFinished { get; set; }
+    public Matchresult[] matchResults { get; set; }
+    public Goal[] goals { get; set; }
+    public object location { get; set; }
+    public object numberOfViewers { get; set; }
+}
 
-        public Team Team1 { get; set; }
-        public Team Team2 { get; set; }
-        public List<MatchResult> MatchResults { get; set; }
-        public DateTime MatchDateTime { get; set; }
-        public int MatchID { get; set; }
-        public bool MatchIsFinished { get; set; }
-        public Group Group { get; set; }
+public class Group
+{
+    public string groupName { get; set; }
+    public int groupOrderID { get; set; }
+    public int groupID { get; set; }
+}
 
-        public MatchResult FinalResult 
-        {
-            get 
-            {
-                foreach (var m in MatchResults)
-                    if (m.ResultTypeID == 2)
-                        return m;
+public class Team
+{
+    public int teamId { get; set; }
+    public string teamName { get; set; }
+    public string shortName { get; set; }
+    public string teamIconUrl { get; set; }
+    public object teamGroupName { get; set; }
+}
 
-                return null;
-            }
-        }
-    }
+public class Matchresult
+{
+    public int resultID { get; set; }
+    public string resultName { get; set; }
+    public int pointsTeam1 { get; set; }
+    public int pointsTeam2 { get; set; }
+    public int resultOrderID { get; set; }
+    public int resultTypeID { get; set; }
+    public string resultDescription { get; set; }
+}
+
+public class Goal
+{
+    public int goalID { get; set; }
+    public int scoreTeam1 { get; set; }
+    public int scoreTeam2 { get; set; }
+    public int matchMinute { get; set; }
+    public int goalGetterID { get; set; }
+    public string goalGetterName { get; set; }
+    public bool isPenalty { get; set; }
+    public bool isOwnGoal { get; set; }
+    public bool isOvertime { get; set; }
+    public object comment { get; set; }
 }
--- 14/Bundesliga/Bundesliga/Program.cs
+++ 14/Bundesliga/Bundesliga/Program.cs
@@ -1,34 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+﻿using System.Net.Http.Json;
 
-namespace Bundesliga
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            PrintAverageHomeGoals();
-            Console.ReadLine();
-        }
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://api.openligadb.de");
 
-        public static async void PrintAverageHomeGoals()
-        {
-            var matchList = await LoadMatches();
-            Console.WriteLine("Durchschn. Heimtore={0:F2}", matchList.AverageHomeGoals);
-        }
-
-        public static async Task<MatchList> LoadMatches() 
-        {
-            var client = new HttpClient();
-            var result = await client.GetStringAsync("https://www.openligadb.de/api/getmatchdata/bl1/2017");
-
-            JArray array = JArray.Parse(result);
-            var matches = array.ToObject<List<Match>>();
-
-            return new MatchList(matches);
-        }
-    }
-}
+var matches = await client.GetFromJsonAsync<IEnumerable<Match>>("/getmatchdata/bl1/2023/1");
+foreach (var m in matches)
+    Console.WriteLine(m.Team1.shortName + " -- " + m.Team2.shortName);
