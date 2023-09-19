850f6aa37c23456b316d6b963d8091afa6e4b62c
unknown <alexander.stuckenholz@gmail.com>
2023-09-19 17:15:29 +0200

    Working

---
+++ 14/RandomDog/RandomDog/DogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RandomDog;
+
+public class DogService
+{
+    private HttpClient client;
+
+    public DogService(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task<ImageSource> LoadDog()
+    {
+        var dog = await client.GetFromJsonAsync<DogMessage>("https://dog.ceo/api/breeds/image/random");
+        return dog.IsSuccess ? new BitmapImage(new Uri(dog.Message)) : null;
+    }
+}
