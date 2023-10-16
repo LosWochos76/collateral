7bcae10566d656bd50387ebc4f89c2db8efcf835
Alexander Stuckenholz <alexander.stuckenholz@hshl.de>
2023-10-16 14:38:45 +0200

    Working

---
+++ 06/BinaryTree.cs
@@ -0,0 +1,9 @@
+public class BinaryTree<T> where T : IComparable<T>
+{
+    private TreeNode<T> root;
+
+    public void Add(T value)
+    {
+        
+    }
+}
---
+++ 06/TreeNode.cs
@@ -0,0 +1,11 @@
+public class TreeNode<T> where T : IComparable<T>
+{
+    public T Value { get; set; }
+    public TreeNode<T> Left { get; set; }
+    public TreeNode<T> Right { get; set; }
+
+    public TreeNode(T value)
+    {
+        Value = value;
+    }
+}
