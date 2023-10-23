9642c64dafbc72fd1e210822e9b853317db8d6b1
Alexander Stuckenholz <alexander.stuckenholz@hshl.de>
2023-10-23 12:48:13 +0200

    Working

---
+++ 08/Program.cs
@@ -0,0 +1,16 @@
+﻿/*var list = new ArrayList<int>();
+var rnd = new Random();
+for (int i=0; i<10; i++)
+    list.Add(rnd.Next(100));
+
+list.SelectionSort();
+list.Print();*/
+
+/*var list = new LinkedList<int>();
+var rnd = new Random();
+for (int i=0; i<10; i++)
+    list.Enqueue(rnd.Next(100));
+
+list.SelectionSort();
+list.Print();*/
+
