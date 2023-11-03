a56df9c213d66ba6bbca05455f343b54e5b89a5a
Alexander Stuckenholz <alexander.stuckenholz@hshl.de>
2023-11-03 12:43:08 +0100

    Working

--- 08/Program.cs
+++ 08/Program.cs
@@ -14,10 +14,19 @@ for (int i=0; i<10; i++)
 list.SelectionSort();
 list.Print();*/
 
-var list = new ArrayList<int>();
+/*var list = new ArrayList<int>();
 var rnd = new Random();
 for (int i=0; i<10; i++)
     list.Add(rnd.Next(100));
 
 list.BubbleSort();
-list.Print();
+list.Print();*/
+
+var rnd = new Random();
+int[] zahlen = new int[10];
+for (int i=0; i<10; i++)
+    zahlen[i] = rnd.Next(100);
+
+var sorted = MergeSort.Sort(zahlen);
+for (int i=0; i<10; i++)
+    Console.WriteLine(sorted[i]);
