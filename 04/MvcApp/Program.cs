64a042c8186b43cf5fa4ce0efc98603abe2e0877
Alexander Stuckenholz <alexander.stuckenholz@2motion.net>
2024-03-27 17:08:19 +0100

    update

---
+++ 04/MvcApp/Program.cs
@@ -0,0 +1,29 @@
+using System.Net;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = new HostBuilder();
+        builder.UseContentRoot(Directory.GetCurrentDirectory());
+
+        builder.ConfigureLogging(logging => {
+            logging.AddConsole();
+        });
+
+        builder.ConfigureAppConfiguration((hostingContext, config) => {
+            var env = hostingContext.HostingEnvironment;
+            config.AddJsonFile("appsettings.json", optional: false);
+            config.AddJsonFile($"appsettings.{env.EnvironmentName}.json");
+            config.AddEnvironmentVariables();
+            config.AddCommandLine(args);
+        });
+
+        builder.ConfigureWebHostDefaults(webBuilder => {
+            webBuilder.UseStartup<Startup>();
+        });
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
---
+++ 04/MvcApp/Startup.cs
@@ -0,0 +1,34 @@
+public class Startup
+{
+    public IConfiguration Configuration { get; set; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpointBuilder => 
+        {
+            endpointBuilder.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+    }
+}
