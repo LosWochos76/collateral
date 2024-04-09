29b8e27e8a3ed863ebdfb73db5cd77f320ec8a98
Alexander Stuckenholz <alexander.stuckenholz@2motion.net>
2024-04-09 18:20:55 +0200

    update

--- 04/MvcApp/Controllers/HomeController.cs
+++ 04/MvcApp/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcApp.Controllers;
 
-public class HomeController : ControllerBase
+public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
@@ -12,14 +11,50 @@ public class HomeController : ControllerBase
         _logger = logger;
     }
 
-    [Route("/")]
     public IActionResult Index()
     {
-        return new ContentResult() 
-        { 
-            Content = "Hello, World!", ContentType = "text/html" 
+        return Content("Hello, World!");
+    }
+
+    public IActionResult Redirect()
+    {
+        return Redirect("/");
+    }
+
+    public IActionResult Error()
+    {
+        return NotFound();
+        //return StatusCode(StatusCodes.Status404NotFound);
+    }
+
+    public IActionResult ToDo()
+    {
+        var obj = new {
+            ID = Guid.NewGuid(),
+            Title = "Test",
+            Completion = 0.5,
+            Description = "A new ToDo-Item"
         };
 
-        return Content("Hello, World!");
+        return Ok(obj);
+    }
+
+    public IActionResult About()
+    {
+        var items = new[] { "First", "Second", "Third" };
+        return View(items); // delivers content of /Views/Home/About.cshtml
+    }
+
+    public async Task<IActionResult> Login()
+    {
+        var context = HttpContext;
+        var form = await context.Request.ReadFormAsync();
+        var username = form["username"];
+        var password = form["password"];
+
+        if (username == "admin" && password == "secret")
+            return Redirect("/data");
+        else
+            return View();
     }
 }
