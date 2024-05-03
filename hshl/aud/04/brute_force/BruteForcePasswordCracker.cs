63db38a8ded46748410332bb264969f80d61ee36
Alexander Stuckenholz <alexander.stuckenholz@2motion.net>
2024-05-03 12:49:56 +0200

    update

--- hshl/aud/04/brute_force/BruteForcePasswordCracker.cs
+++ hshl/aud/04/brute_force/BruteForcePasswordCracker.cs
@@ -5,45 +5,62 @@ using System.Text;
 public class BruteForcePasswordCracker
 {
     static string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    static string currentAttempt = string.Empty;
     static int attempts = 0;
 
-    static string ComputeSha256Hash(string rawData)
+    private static string ComputeSha256Hash(string rawData)
     {
         using (SHA256 sha256Hash = SHA256.Create())
         {
             byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
-
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
-            {
                 builder.Append(bytes[i].ToString("x2"));
-            }
 
             return builder.ToString();
         }
     }
 
-    public static bool Login(string password)
+    private static bool IsEqual(string password, string hash)
     {
-        return ComputeSha256Hash(password) == "095fffbf92f0fcff322081a2c3bd09b56f91b8bc7cda7b73d23d86d102f7cf27";
+        return ComputeSha256Hash(password) == hash;
     }
 
-    public static void Main(string[] args)
+    // Als test sei hash = "095fffbf92f0fcff322081a2c3bd09b56f91b8bc7cda7b73d23d86d102f7cf27"
+    public static string CrackPassword(string hash)
     {
+        string attempt = "";
+
         do
         {
-            currentAttempt = NextCandidate();
+            attempt = NextCandidate(attempt, 0);
+            //Console.WriteLine(currentAttempt);
             attempts++;
         }
-        while (!Login(currentAttempt));
+        while (!IsEqual(attempt, hash));
 
-        Console.WriteLine($"Passwort gefunden: {currentAttempt}");
+        Console.WriteLine($"Passwort gefunden: {attempt}");
         Console.WriteLine($"Versuche benötigt: {attempts}");
+        return attempt;
     }
 
-    public static string NextCandidate()
+    private static char NextChar(char c)
     {
-        // Hier fehlt etwas
+        return characters[(characters.IndexOf(c) + 1) % characters.Length];
+    }
+
+    private static string NextCandidate(string current, int pos)
+    {
+        if (pos > current.Length - 1)
+            return current + characters[0];
+
+        var chars = current.ToCharArray();
+        var next_char = NextChar(chars[pos]);
+        chars[pos] = next_char;
+        current = new string(chars);
+
+        if (next_char == 'a')
+            current = NextCandidate(current, pos + 1);
+
+        return current; 
     }
 }
--- hshl/aud/04/brute_force/Program.cs
+++ hshl/aud/04/brute_force/Program.cs
@@ -3,10 +3,12 @@ hvm.FindSolutions();*/
 
 //Console.WriteLine(Maze.NumberOfPaths(3,3));
 
-var maze = new Maze(new int[,] {
+/*var maze = new Maze(new int[,] {
     {  0,  3, 10,  3,  1,  2 },
     { 15,  7, 17, 11,  3,  5 },
     { 21,  9,  4,  2,  1,  2 },
     {  3,  7,  6,  3,  5,  0 } });
 
-Console.WriteLine("BEST: {0}", maze.Solve());
+Console.WriteLine("BEST: {0}", maze.Solve());*/
+
+BruteForcePasswordCracker.CrackPassword("095fffbf92f0fcff322081a2c3bd09b56f91b8bc7cda7b73d23d86d102f7cf27");
